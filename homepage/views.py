0812a4ed9f6e3ef2c3e219daf2b16274ee228ec8
Sofia-Skovoronskaia <79004820+Sofia-Skovoronskaia@users.noreply.github.com>
2021-04-01 22:59:23 +0300

    first commit

---
+++ homepage/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from .models import friends_db, icecream_db
+from .services import what_weather, what_temperature, what_conclusion
+
+
+def index(request):
+    icecreams = ''
+    friends = ''
+    city_weather = ''
+    friend_output = ''
+    selected_icecream = ''
+    conclusion = ''
+
+    for friend in friends_db:
+        friends += (f'<input type="radio" name="friend"'
+                    f' required value="{friend}">{friend}<br>')
+
+    for i in range(len(icecream_db)):
+        ice_form = (f'<input type="radio" name="icecream" required'
+                    f' value="{icecream_db[i]["name"]}">{icecream_db[i]["name"]}')
+
+        ice_link = f'<a href="icecream/{i}/">Узнать состав</a>'
+        icecreams += f'{ice_form} | {ice_link} <br>'
+
+    if request.method == 'POST':
+        selected_friend = request.POST.get('friend', False)
+        selected_icecream = request.POST.get('icecream', False)
+
+        city = friends_db[selected_friend]
+        weather = what_weather(city)
+        parsed_temperature = what_temperature(weather)
+
+        conclusion = what_conclusion(parsed_temperature)
+        friend_output = f'{selected_friend}, тебе прислали {selected_icecream}!'
+        city_weather = f'В городе {city} погода: {weather}'
+
+    context = {
+        'icecreams': icecreams,
+        'friends': friends,
+        'friend_output': friend_output,
+        'city_weather': city_weather,
+        'conclusion': conclusion,
+
+    }
+    return render(request, 'homepage/index.html', context)
+
